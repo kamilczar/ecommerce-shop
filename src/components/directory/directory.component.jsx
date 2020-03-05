12e66c2a08d1621339dc24c58845c668bdb67b1b
kamilczar <kamilc.dev@gmail.com>
2020-03-05 20:48:49 +0000

    Added routing

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './directory.styles.scss'
-import { MenuItem } from '../menu-item/menu-item.component'
+import MenuItem from '../menu-item/menu-item.component'
 
 class Directory extends React.Component {
     constructor() {
@@ -49,10 +49,9 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                {
-                   this.state.sections.map(({title, imageUrl, id, size}) => (
-                       <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
-                   ))
-               } 
+                   this.state.sections.map(({ id, ...otherSectionProps }) => (
+                       <MenuItem key={id} {...otherSectionProps} />
+                   ))} 
             </div>
         )
     }
