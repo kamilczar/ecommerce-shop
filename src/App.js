511671ea619debadef9350f00d6ca76b468c916d
kamilczar <kamilc.dev@gmail.com>
2020-04-26 22:41:19 +0100

    Set up sign up through email

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import AuthPage from './pages/auth/auth.component';
-import { auth } from './utils/firebase';
+import { auth, createUserProfileDocument } from './utils/firebase';
 
 class App extends React.Component {
   constructor() {
@@ -22,9 +22,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user})
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = createUserProfileDocument(userAuth);
+
+        (await userRef).onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          })
+        })
+      }
+      this.setState({ currentUser: userAuth });
+    });
   }
 
   componentWillUnmount() {
--- src/pages/auth/auth.component.jsx
+++ src/pages/auth/auth.component.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './auth.style.scss';
 
 import SignInComponent from '../../components/sign-in/sign-in.component';
+import SignUpComponent from '../../components/sign-up/sign-up.component';
 
 const AuthPage = () => (
     <div className='auth-component'>
         <SignInComponent />
+        <SignUpComponent />
     </div>
 );
 
