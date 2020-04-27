9eb37b54dfa03ae20f7109dfb7b2eeac1527473a
kamilczar <kamilc.dev@gmail.com>
2020-04-27 22:39:08 +0100

    Added redirects and added user types for redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/first */
 import React from 'react';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import Header from './components/header/header.component';
 import './App.css';
 import { connect } from 'react-redux';
@@ -42,15 +42,28 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
-          <Route exact path='/signin' component={AuthPage} />
+          <Route
+            exact 
+            path='/signin' 
+            render={() => 
+              this.props.currentUser ? (
+                <Redirect to='/' />) : (
+                <AuthPage />
+                )
+              }
+              />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
