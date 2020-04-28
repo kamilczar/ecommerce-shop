1ced032ef0d14a0febc23ca1ac7adb5d47d7490c
kamilczar <kamilc.dev@gmail.com>
2020-04-29 00:21:52 +0100

    Updated shop data, updated cart utils to group items in cart reducer

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,23 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import { ReactComponent as ShoppingIcon } from '../../assets/cart.svg';
-import { connect } from 'react-redux';
-import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { ReactComponent as ShoppingIcon } from "../../assets/cart.svg";
+import { connect } from "react-redux";
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
-import './cart-icon.styles.scss';
+import "./cart-icon.styles.scss";
 
 const CartIcon = ({ toggleCartHidden }) => (
-    <div className='cart-icon' onClick={toggleCartHidden} >
-        <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'> 0 </span>
-    </div>
+  <div className="cart-icon" onClick={toggleCartHidden}>
+    <ShoppingIcon className="shopping-icon" />
+    <span className="item-count"> 0 </span>
+  </div>
 );
 
-const mapDispatchToProps = dispatch => ({
-    toggleCartHidden: () => dispatch(toggleCartHidden())
+const mapDispatchToProps = (dispatch) => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(
-    null,
-    mapDispatchToProps
-)(CartIcon);
+export default connect(null, mapDispatchToProps)(CartIcon);
--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import CollectionItem from '../collection-item/collection-item.component';
+import React from "react";
+import CollectionItem from "../collection-item/collection-item.component";
 
-import './preview-component.style.scss'
+import "./preview-component.style.scss";
 
-const CollectionPreview = ({ title, items}) => (
-    <div>
-        <h1 className='title'>{ title.toUpperCase() }</h1>
-        <div className='preview'>
-            { items
-            .filter((item, idx) => idx < 4)
-            .map(({id, ...itemProps}) => (
-                <CollectionItem key={ id } { ...itemProps } />
-            ))}
-        </div> 
+const CollectionPreview = ({ title, items }) => (
+  <div>
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
-)
+  </div>
+);
 
 export default CollectionPreview;
