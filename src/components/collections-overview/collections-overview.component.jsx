09c3847964e3a194d0fef2eebaffdda424493d69
kamilczar <kamilc.dev@gmail.com>
2020-05-02 00:21:05 +0100

    Added stores to shop collections and shop items

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {connect} from "react-redux";
+import {createStructuredSelector} from "reselect";
+import {selectCollections} from "../../redux/shop/shop.selector";
+
+import './collections-overview.styles.scss'
+import CollectionPreview from "../preview-collection/preview-collection.component";
+
+const CollectionsOverview = ({collections}) => (
+    <div className='collections-overview'>
+        {
+            collections.map(({id, ...otherCollectionProps}) => (
+                <CollectionPreview
+                    key={id}
+                    {...otherCollectionProps}
+                />
+            ))
+        }
+    </div>
+)
+
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+})
+
+export default connect(mapStateToProps)(CollectionsOverview);
